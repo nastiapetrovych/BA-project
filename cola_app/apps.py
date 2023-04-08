10aef85ae738d3fc0afaebdf2fa78b00b105da12
rostik <rostyslav.kostiuk@ucu.edu.ua>
2023-04-09 02:21:54 +0300

    Initial commit

---
+++ cola_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ColaAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cola_app'
---
+++ cola_app/views.py
@@ -0,0 +1,12 @@
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+
+def index(request):
+    return render(request, "cola_app/index.html")
